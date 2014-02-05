6efb4a838e6404eff6f2e4bf6d802374b0925bfe
Christopher Bleakley <cbleakley@Christopher-Bleakleys-MacBook-Pro.local>
2014-02-04 22:55:58 -0800

    initial commit

---
+++ extension/background.js
@@ -0,0 +1,57 @@
+var bookmarks = [];
+
+function getRandomBookmark() {
+  return bookmarks[Math.floor(Math.random()*bookmarks.length)];
+}
+
+function loadRandomBookmarkNewTab() {
+  chrome.tabs.create({
+    url: getRandomBookmark().url
+  });
+}
+
+function loadRandomBookmarkSameTab(bookmark) {
+  chrome.tabs.update({
+    url: getRandomBookmark().url
+  });
+}
+
+function buildBookmarks() {
+  console.log("rebuilding bookmark list");
+  chrome.bookmarks.getTree(
+    function traverseBookmarks(bookmarkTreeNodes) {
+      for (var i = 0; i < bookmarkTreeNodes.length; i++) {
+        bookmarks.push(bookmarkTreeNodes[i]);
+        if (bookmarkTreeNodes[i].children) {
+          traverseBookmarks(bookmarkTreeNodes[i].children);
+        }
+      }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  buildBookmarks();
+
+  chrome.bookmarks.onCreated.addListener(function () {
+    buildBookmarks();
+  });
+  chrome.bookmarks.onRemoved.addListener(function () {
+    buildBookmarks();
+  });
+  chrome.bookmarks.onChanged.addListener(function () {
+    buildBookmarks();
+  });
+  
+  chrome.browserAction.onClicked.addListener( function(tab){
+    loadRandomBookmark();
+  });
+
+  chrome.commands.onCommand.addListener(function (command) {
+    if (command === 'random-bookmark-new-tab') {
+      loadRandomBookmarkNewTab();  
+    }
+    if (command === 'random-bookmark-same-tab') {
+      loadRandomBookmarkSameTab();  
+    } 
+  });
+});
